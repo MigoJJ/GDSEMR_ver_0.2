6e5b076ee849045a77f412f30e673872a6f6a792
migowj <migowj@migowj-VMware>
2025-10-26 11:20:32 +0900

    Vaccine

---
+++ app/src/main/java/com/emr/gds/fourgate/vaccine/VaccineConstants.java
@@ -0,0 +1,53 @@
+package com.emr.gds.fourgate.vaccine;
+
+public final class VaccineConstants {
+
+    /**
+     * Private constructor to prevent instantiation of this utility class.
+     */
+    private VaccineConstants() {}
+
+    public static final String[] UI_ELEMENTS = {
+            "### Respiratory Vaccines",
+            "Sanofi's Vaxigrip® Vaccine(H3N2)",
+            "GC Flu Plus (H1N1, H3N2, Victoria lineage)® vaccine [NIP]",
+//            "Pneumovax 23 (PPSV23, pneumococcal polysaccharide vaccine)",
+//            "Pneumovax 20 (PPSV20, pneumococcal polysaccharide vaccine)",
+            "Prevena 20 (pneumococcal vaccine (PCV20))",
+            "Prevena 13 (pneumococcal vaccine (PCV13))",
+            "COVID-19 vaccine (mRNA, viral vector, or protein subunit)",
+//            "RSV vaccine (Arexvy, Abrysvo)",
+            
+            "### Travel / Endemic Disease Vaccines",
+            "MMR (Measles, Mumps, Rubella)",
+            "Varicella (Chickenpox) vaccine",
+            "Japanese Encephalitis vaccine",
+            "Yellow Fever vaccine",
+            "Typhoid vaccine (oral or injectable)",
+            "Meningococcal vaccine (MenACWY)",
+//            "Meningococcal vaccine (MenB)",
+//            "Rabies vaccine (pre-exposure prophylaxis)",
+
+            "### Occupational / High-Risk Vaccines",
+            "HPV vaccine (Gardasil 9, adults up to age 45)",
+            "Anthrax vaccine",
+            "Smallpox/Mpox vaccine (JYNNEOS)",
+
+            "### Booster / Additional Doses",
+            "HAV vaccine #1/2",
+            "HBV vaccine #1/3",
+//            "HBV vaccine #3/3",
+            "Shingles Vaccine (Shingrix) #1/2",
+            "TdaP (Tetanus, Diphtheria, Pertussis)",
+            "Td booster (Tetanus, Diphtheria)",
+
+            "### Regional / Seasonal Vaccines",
+//            "Tick-borne Encephalitis vaccine",
+            "Cholera vaccine",
+//            "Polio vaccine (IPV, for travelers to endemic areas)",
+
+            "### Actions",
+            "Side Effect",
+            "Quit"
+    };
+}
